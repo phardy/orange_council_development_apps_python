f1a5b8b82a76587bc1205532d74ba20ff362e0f9
Peter Hardy <peter@hardy.dropbear.id.au>
2024-07-19 23:44:12 +1000

    rough stab at grabbing the html

--- scraper.py
+++ scraper.py
@@ -9,7 +9,8 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)s:%(message)s')
 
 def parse_feed():
-    feed_url="https://www.orange.nsw.gov.au/category/das-on-exhibition/feed/"
+#    feed_url="https://www.orange.nsw.gov.au/category/das-on-exhibition/feed/"
+    feed_url="https://www.orange.nsw.gov.au/category/das-on-exhibition/"
     # Look mum, I'm an ipad!
     headers={
         "User-Agent":"Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"
@@ -18,7 +19,8 @@ def parse_feed():
     req = urllib.request.Request(url=feed_url,
                                  headers=headers)
     fh = urllib.request.urlopen(req)
-    page=BeautifulSoup(fh, "xml")
+    # page=BeautifulSoup(fh, "xml")
+    page=BeautifulSoup(fh)
     items=page.findAll('item')
     for item in items:
         title=item.title
