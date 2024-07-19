17541ad40ce95babe19e6d8f686cd0c42254d233
Peter Hardy <peter@hardy.dropbear.id.au>
2024-07-19 22:47:41 +1000

    retrieve the page and log titles

--- scraper.py
+++ scraper.py
@@ -1,24 +1,21 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import logging
+import os
+os.environ["SCRAPERWIKI_DATABASE_NAME"] = "sqlite:///data.sqlite"
+import scraperwiki
+from bs4 import BeautifulSoup
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s:%(message)s')
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+feed_url="https://www.orange.nsw.gov.au/category/das-on-exhibition/feed/"
+
+def parse_feed():
+    feed = scraperwiki.scrape(feed_url)
+    page=BeautifulSoup(feed, "xml")
+    items=page.findAll('item')
+    for item in items:
+        title=item.title
+        logging.info(f"Item titled {title}")
+
+if __name__ == "__main__":
+    parse_feed()
